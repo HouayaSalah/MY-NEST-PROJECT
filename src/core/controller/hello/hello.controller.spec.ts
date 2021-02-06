376ce65c56ff6ad282b167baeaa645aedd55ef6a
HOUAYA SALAH <shouaya@wissal-sys.com>
2021-02-06 23:13:20 +0100

    add dockerfile / fix fastify docker conf

--- src/core/controller/hello/hello.controller.spec.ts
+++ src/core/controller/hello/hello.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HelloService } from 'src/core/service/hello/hello.service';
 import { HelloController } from './hello.controller';
 
 describe('HelloController', () => {
@@ -7,12 +8,15 @@ describe('HelloController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HelloController],
+      providers: [HelloService]
     }).compile();
 
     controller = module.get<HelloController>(HelloController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('hi', () => {
+  it('should return "HELLO HOUAYA SALAH"', () => {
+    expect(controller.getHi()).toBe('HELLO HOUAYA SALAH');
   });
+});
 });
